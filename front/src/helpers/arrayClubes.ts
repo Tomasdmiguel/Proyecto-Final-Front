c2ada2675554393585a82190c6c904df65aa85d1
psmeier <psmeier05@gmail.com>
2024-06-27 17:13:08 -0300

    arrays canchas

--- front/src/helpers/arrayClubes.ts
+++ front/src/helpers/arrayClubes.ts
@@ -77,3 +77,81 @@ export const arrayClubes3 = [
     cantidadCanchas: 5,
   },
 ];
+export const arrayCanchas = [
+  {
+    id: 1,
+    nombre: "Cancha 1",
+    tipo: "sintético",
+    jugadores: 10,
+  },
+  {
+    id: 2,
+    nombre: "Cancha 2",
+    tipo: "sintético",
+    jugadores: 10,
+  },
+  {
+    id: 3,
+    nombre: "Cancha 3",
+    tipo: "sintético",
+    jugadores: 10,
+  },
+  {
+    id: 4,
+    nombre: "Cancha 4",
+    tipo: "sintético",
+    jugadores: 10,
+  },
+];
+export const arrayCanchas2 = [
+  {
+    id: 5,
+    nombre: "Cancha 5",
+    tipo: "sintético",
+    jugadores: 10,
+  },
+  {
+    id: 6,
+    nombre: "Cancha 6",
+    tipo: "sintético",
+    jugadores: 10,
+  },
+  {
+    id: 7,
+    nombre: "Cancha 7",
+    tipo: "sintético",
+    jugadores: 10,
+  },
+  {
+    id: 8,
+    nombre: "Cancha 8",
+    tipo: "sintético",
+    jugadores: 10,
+  },
+];
+export const arrayCanchas3 = [
+  {
+    id: 9,
+    nombre: "Cancha 9",
+    tipo: "sintético",
+    jugadores: 10,
+  },
+  {
+    id: 10,
+    nombre: "Cancha 10",
+    tipo: "sintético",
+    jugadores: 10,
+  },
+  {
+    id: 11,
+    nombre: "Cancha 11",
+    tipo: "sintético",
+    jugadores: 10,
+  },
+  {
+    id: 12,
+    nombre: "Cancha 12",
+    tipo: "sintético",
+    jugadores: 10,
+  },
+];
