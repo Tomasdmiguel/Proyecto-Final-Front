38c74adc09898c2a0f3af679e9d27bb07c647128
tomasdmiguel <tomasdeskate@hotmail.es>
2024-06-30 19:35:09 -0300

    sedes

--- front/src/components/Sede/FormSede.tsx
+++ front/src/components/Sede/FormSede.tsx
@@ -4,10 +4,11 @@ import { useState } from "react";
 import Link from "next/link";
 import Swal from "sweetalert2";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { IUser } from "@/interface/IUser";
 
 //*Importacion para controlar el form
 import { CSede } from "@/helpers/Controllers/CSede";
-
 
 //*Importacion para crear sede
 import { fetchFormSede } from "@/service/ApiFormSede";
@@ -17,8 +18,15 @@ const FormSede = () => {
     name: "",
     location: "",
     description: "",
-    
   });
+  const [userData, setUserData] = useState<IUser | undefined>();
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const userData = localStorage.getItem("usuarioSesion");
+      setUserData(JSON.parse(userData!));
+    }
+  }, []);
 
   const route = useRouter();
   //*Funcion que guarda los cambios
@@ -36,24 +44,32 @@ const FormSede = () => {
 
     if (CSede(datoSede)) {
       try {
-        const response = await fetchFormSede(datoSede);
-        if (response.success) {
-          Swal.fire({
-            icon: "success",
-            title: "Se creo la sede con exito",
-          });
-          console.log("Sede creada exitosamente");
-          
-          route.push("/Dashboard");
+        if (userData) {
+          const response = await fetchFormSede(datoSede, userData);
+          if (response.success) {
+            Swal.fire({
+              icon: "success",
+              title: "Se creó la sede con éxito",
+            });
+            console.log("Sede creada exitosamente");
+            route.push("/Dashboard");
+          } else {
+            console.error(
+              "Error al crear la sede, la petición a la API fue buena pero por algo no se pudo crear:",
+              response.message
+            );
+            Swal.fire({
+              icon: "error",
+              title: "Error al crear el formulario, revisa los datos",
+              text: response.message,
+            });
+          }
         } else {
-          console.error(
-            "Error al crear la sede, la peticion a la api fue buena pero por algo no se pudo crear:",
-            response.message
-          );
+          console.error("Error: No se encontraron datos de usuario válidos.");
           Swal.fire({
             icon: "error",
-            title: "Error al crear el formulario revisa los datos",
-            text: response.message,
+            title: "Error de usuario",
+            text: "No se encontraron datos de usuario válidos, intenta iniciar sesión nuevamente",
           });
         }
       } catch (error) {
@@ -65,7 +81,7 @@ const FormSede = () => {
         });
       }
     } else {
-      //Se maneja el error de la validacion si salio todo mal
+      // Se maneja el error de la validación si salió todo mal
       Swal.fire({
         icon: "warning",
         title: "Datos inválidos",
@@ -76,58 +92,60 @@ const FormSede = () => {
 
   return (
     <div className="bg-main max-w-md w-full p-8 rounded-lg shadow-lg text">
-    <h1 className="text-terciario-white text-center text-3xl font-bold mb-6">
-      RESERVA GOL
-    </h1>
-  
-    <p className="text-secundario text-center mb-4">
-      Bienvenido, crea tu sede para que los jugadores puedan ver tus canchas
-    </p>
-  
-    <form onSubmit={handleSubmit}>
-      <div className="mb-4">
-        <label htmlFor="name" className="block text-terciario-white mb-2">
-          Nombre
-        </label>
-        <input
-          type="text"
-          name="name"
-          value={datoSede.name}
-          placeholder="Escribi tu nombre"
-          onChange={hanldeChange}
-          className="w-full p-3 rounded-lg bg-white text-black focus:border-yellow-600"
-        />
-      </div>
-  
-      <div className="mb-4">
-        <label htmlFor="location" className="block text-terciario-white mb-2">
-          Locacion
-        </label>
-        <input
-          type="text"
-          name="location"
-          value={datoSede.location}
-          placeholder="Ponga su locacion"
-          onChange={hanldeChange}
-          className="w-full p-3 rounded-lg bg-white text-black focus:border-yellow-600"
-        />
-      </div>
-  
-      <div className="mb-4">
-        <label htmlFor="description" className="block text-terciario-white mb-2">
-          Descripcion
-        </label>
-        <input
-          type="text"
-          name="description"
-          value={datoSede.description}
-          placeholder="Descripcion"
-          onChange={hanldeChange}
-          className="w-full p-3 rounded-lg bg-white text-black focus:border-yellow-600"
-        />
-      </div>
-  
-      {/* <div className="mb-6">
+      <h1 className="text-terciario-white text-center text-3xl font-bold mb-6">
+        RESERVA GOL
+      </h1>
+
+      <p className="text-secundario text-center mb-4">
+        Bienvenido, crea tu sede para que los jugadores puedan ver tus canchas
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-terciario-white mb-2">
+            Nombre
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={datoSede.name}
+            placeholder="Escribi tu nombre"
+            onChange={hanldeChange}
+            className="w-full p-3 rounded-lg bg-white text-black focus:border-yellow-600"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="location" className="block text-terciario-white mb-2">
+            Locacion
+          </label>
+          <input
+            type="text"
+            name="location"
+            value={datoSede.location}
+            placeholder="Ponga su locacion"
+            onChange={hanldeChange}
+            className="w-full p-3 rounded-lg bg-white text-black focus:border-yellow-600"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="description"
+            className="block text-terciario-white mb-2">
+            Descripcion
+          </label>
+          <input
+            type="text"
+            name="description"
+            value={datoSede.description}
+            placeholder="Descripcion"
+            onChange={hanldeChange}
+            className="w-full p-3 rounded-lg bg-white text-black focus:border-yellow-600"
+          />
+        </div>
+
+        {/* <div className="mb-6">
         <label htmlFor="img" className="block text-terciario-white mb-2">
           Imagen
         </label>
@@ -140,22 +158,21 @@ const FormSede = () => {
           className="w-full p-3 rounded-lg bg-white text-black focus:border-yellow-600"
         />
       </div> */}
-  
-      <button
-        type="submit"
-        className="w-full border border-secundario text-terciario-white p-3 rounded-lg hover:bg-yellow-600"
-      >
-        Crear sede
-      </button>
-    </form>
-  
-    <p className="text-terciario-white text-center mt-4">
-      Si ya tienes una sede creada, ingresa para crear tu cancha.
-    </p>
-    <Link href="/FormCancha" className="text-secundario text-center">
-      <p>Ingresa</p>
-    </Link>
-  </div>
+
+        <button
+          type="submit"
+          className="w-full border border-secundario text-terciario-white p-3 rounded-lg hover:bg-yellow-600">
+          Crear sede
+        </button>
+      </form>
+
+      <p className="text-terciario-white text-center mt-4">
+        Si ya tienes una sede creada, ingresa para crear tu cancha.
+      </p>
+      <Link href="/FormCancha" className="text-secundario text-center">
+        <p>Ingresa</p>
+      </Link>
+    </div>
   );
 };
 export default FormSede;
--- front/src/service/ApiFormSede.ts
+++ front/src/service/ApiFormSede.ts
@@ -1,15 +1,19 @@
 import { IFormSede } from "@/interface/IFormSede";
+
+import { IUser } from "@/interface/IUser";
+
 const apiKey = process.env.NEXT_PUBLIC_API_URL;
 
-export const fetchFormSede = async (data: IFormSede) => {
+export const fetchFormSede = async (data: IFormSede, userDB:IUser ) => {
   try {
     const response = await fetch(`${apiKey}/sede`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify({ data: data, userDB}),
     });
+    console.log({ data: data, userDB: userDB})
     if (response.ok) {
       return { success: true };
     } else {
