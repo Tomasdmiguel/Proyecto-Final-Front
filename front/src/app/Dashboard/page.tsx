7e00c0ea69fa4ba3b86a5ec9a41d61902ff4297e
tomasdmiguel <tomasdeskate@hotmail.es>
2024-07-01 17:11:57 -0300

    Auth google

--- front/src/app/Dashboard/page.tsx
+++ front/src/app/Dashboard/page.tsx
@@ -5,32 +5,34 @@ import React, { useEffect, useState } from "react";
 import imgUsuario from "@/assets/user_profile_man-256.webp";
 import { useRouter } from "next/navigation";
 import { ISede } from "@/interface/ISedes";
-import { IUser } from "@/interface/IUser";
 import { IUserSession } from "@/interface/context";
 import Swal from "sweetalert2";
 
 export default function Dashboard() {
   const router = useRouter();
-  const [userData, setUserData] = useState<IUserSession>();
+  const [userData, setUserData] = useState<IUserSession | null>(null);
   const [sede, setSedes] = useState<ISede[]>([]);
 
   useEffect(() => {
     if (typeof window !== "undefined" && window.localStorage) {
       const userData = localStorage.getItem("usuarioSesion");
-      setUserData(JSON.parse(userData!));
+      if (userData) {
+        setUserData(JSON.parse(userData));
+      }
     }
   }, []);
 
-  // Pongo un alert de momento para cerrar sesion
+  // Función para cerrar sesión
   const handleLogOut = () => {
     localStorage.removeItem("usuarioSesion");
     Swal.fire({
       icon: "success",
-      title: "Se cerro secion exitosamente",
+      title: "Se cerró sesión exitosamente",
     });
     router.push("/Login");
   };
 
+  // Función para redirigir a la creación de sede
   const handleCreate = () => {
     router.push("/Formsede");
   };
@@ -44,25 +46,30 @@ export default function Dashboard() {
       <div className="bg-[#F5F7F8] p-8 rounded-lg shadow-xl w-[60%] text-terciario mt-10 text-xl flex flex-row items-center justify-evenly">
         <div className="space-y-8 space-x-4">
           <h1 className="text-3xl font-Marko text-black">
-            Bienvenido, {userData?.userDb.name}!
+            Bienvenido, {userData?.userDb.name || userData?.userDb.displayName}!
           </h1>
           <p className="hover:font-black duration-300 ease-in-out">
             Nombre:{" "}
-            <span className="hover:text-main">{userData?.userDb.name}</span>
+            {userData?.userDb.name ? (
+              <span className="hover:text-main">{userData?.userDb.name}</span>
+            ) : (
+              <span className="hover:text-main">
+                {userData?.userDb.displayName}
+              </span>
+            )}
           </p>
           <p className="hover:font-black duration-300 ease-in-out">
-            Correo electronico:{" "}
+            Correo electrónico:{" "}
             <span className="hover:text-main">{userData?.userDb.email}</span>
           </p>
           <p className="hover:font-black duration-300 ease-in-out">
-            Numero:{" "}
+            {userData?.userDb.phone && "Número: "}
             <span className="hover:text-main">{userData?.userDb.phone}</span>
           </p>
           <button
             className="text-black md:text-lg p-3 rounded-lg border border-x-2 border-y-2 border-secundario hover:border-red-400 hover:shadow-md hover:shadow-black hover:bg-red-600 hover:text-white duration-200 ease-in-out"
-            onClick={handleLogOut}
-          >
-            Cerrar sesion
+            onClick={handleLogOut}>
+            Cerrar sesión
           </button>
         </div>
 
@@ -76,11 +83,10 @@ export default function Dashboard() {
         </p>
 
         <div className="flex flex-col gap-16 text-2xl">
-          {userData?.userDb.sedes.map((sede) => (
+          {userData?.userDb?.sedes?.map((sede) => (
             <div
               key={sede.name}
-              className="w-full max-h-60 rounded-sm shadow-xl hover:shadow-terciario hover:bg-main hover:text-white ease-in-out duration-300 p-4 space-x-4 space-y-6"
-            >
+              className="w-full max-h-60 rounded-sm shadow-xl hover:shadow-terciario hover:bg-main hover:text-white ease-in-out duration-300 p-4 space-x-4 space-y-6">
               <h2 className="font-Marko font-bold text-3xl">{sede.name}</h2>
               <p>Dirección: {sede.location}</p>
               <p>Descripción: {sede.description}</p>
@@ -90,14 +96,12 @@ export default function Dashboard() {
 
         <button
           className="text-black md:text-lg p-3 rounded-lg border border-x-2 border-y-2 border-secundario hover:shadow-md hover:bg-secundario duration-200 ease-in-out mt-8 w-[8vw]"
-          onClick={handleCreate}
-        >
+          onClick={handleCreate}>
           Crear sede
         </button>
         <button
           className="text-black md:text-lg p-3 rounded-lg border border-x-2 border-y-2 border-secundario hover:shadow-md hover:bg-secundario duration-200 ease-in-out mt-8 w-[8vw]"
-          onClick={handleCreateCancha}
-        >
+          onClick={handleCreateCancha}>
           Crear Cancha
         </button>
       </div>
--- front/src/components/Login/FormLogin.tsx
+++ front/src/components/Login/FormLogin.tsx
@@ -1,24 +1,72 @@
-//*Este modulo login es un componente del cliente que lo que hace  es iniciarle secion al usuario
 "use client";
 import { useState } from "react";
 import Link from "next/link";
 import Swal from "sweetalert2";
 import { useRouter } from "next/navigation";
-//*Importacion de Controlador para este formulario
+//*Importación de Controlador para este formulario
 import { CLogin } from "@/helpers/Controllers/CLogin";
-
-//*Importacion de funcion para hacer peticiones para este form
+//*Importación de función para hacer peticiones para este form
 import { fetchLogin } from "@/service/ApiLogin";
+//!Importación para el login por Google
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAxvQJfqH7SpDhf-k5FecgwEOHL8c-zITQ",
+  authDomain: "reservagol-b6cce.firebaseapp.com",
+  projectId: "reservagol-b6cce",
+  storageBucket: "reservagol-b6cce.appspot.com",
+  messagingSenderId: "1091580255495",
+  appId: "1:1091580255495:web:aec6d970530cddc32e39a2",
+  measurementId: "G-EPD836Q6BK",
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
 
 const FormLogin = () => {
   const [data, setData] = useState({
     email: "",
     password: "",
   });
-  const history = useRouter();
-  //*Funcion que guarda los cambios
-  const hanldeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
+  
+  const router = useRouter();
+
+  //!Función para iniciar sesión con Google
+  const callLoginGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        const userDb = result.user;
+        
+        // Guardar usuario en localStorage o manejarlo según tu lógica
+        localStorage.setItem(
+          "usuarioSesion",
+          JSON.stringify({ token, userDb })
+        );
+        
+        Swal.fire({
+          icon: "success",
+          title: "Login exitoso",
+          text: "Sesión iniciada correctamente con Google",
+        });
+        router.push("/");
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: "error",
+          title: "Error de inicio de sesión",
+          text: error.message,
+        });
+      });
+  };
+
+  //*Función que guarda los cambios
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value, name } = event.target;
     setData({
       ...data,
@@ -26,7 +74,7 @@ const FormLogin = () => {
     });
   };
 
-  //*Funcion que envia el formulario
+  //*Función que envía el formulario
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -35,18 +83,18 @@ const FormLogin = () => {
         const response = await fetchLogin(data);
 
         if (response.success) {
-          const {  token,userDb } = response.data;
+          const { token, userDb } = response.data;
           localStorage.setItem(
             "usuarioSesion",
-            JSON.stringify({ token,userDb})
+            JSON.stringify({ token, userDb })
           );
-          
+
           Swal.fire({
             icon: "success",
             title: "Login exitoso",
             text: "Sesión iniciada correctamente",
           });
-          history.push('/')
+          router.push("/");
         } else {
           Swal.fire({
             icon: "error",
@@ -69,61 +117,73 @@ const FormLogin = () => {
       });
     }
   };
+
   return (
-<div className="bg-main max-w-md w-full p-8 rounded-lg shadow-lg text">
-  <h1 className="text-terciario-white text-center text-3xl font-bold mb-6">
-    RESERVA GOL
-  </h1>
+    <div className="bg-main max-w-md w-full p-8 rounded-lg shadow-lg text">
+      <h1 className="text-terciario-white text-center text-3xl font-bold mb-6">
+        RESERVA GOL
+      </h1>
 
-  <p className="text-secundario text-center mb-4">
-    Inicia sesión en tu cuenta
-  </p>
+      <p className="text-secundario text-center mb-4">
+        Inicia sesión en tu cuenta
+      </p>
 
-  <form onSubmit={handleSubmit}>
-    <div className="mb-4">
-      <label htmlFor="email" className="block text-terciario-white mb-2  focus:border-yellow-600">
-        Email
-      </label>
-      <input
-        type="email"
-        name="email"
-        value={data.email}
-        placeholder="Escribí tu email"
-        onChange={hanldeChange}
-        className="w-full p-3 rounded-lg bg-white text-black  focus:border-yellow-600 placeholder-black"
-      />
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-terciario-white mb-2"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            value={data.email}
+            placeholder="Escribí tu email"
+            onChange={handleChange}
+            className="w-full p-3 rounded-lg bg-white text-black"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="password" className="block text-terciario-white mb-2">
+            Contraseña
+          </label>
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            placeholder="Escribe tu contraseña"
+            onChange={handleChange}
+            className="w-full p-3 rounded-lg bg-white text-black"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full border border-secundario text-terciario-white p-3 rounded-lg hover:bg-yellow-600"
+        >
+          Iniciar sesión
+        </button>
+      </form>
+
+      <button
+        onClick={callLoginGoogle}
+        className="w-full mt-4 border border-secundario text-terciario-white p-3 rounded-lg hover:bg-yellow-600"
+      >
+        Iniciar sesión con Google
+      </button>
+
+      <p className="text-terciario-white text-center mt-4">
+        Si no tienes una cuenta
+        <br />
+        <Link className="text-secundario" href="/Register">
+          regístrate
+        </Link>
+      </p>
     </div>
-
-    <div className="mb-4">
-      <label htmlFor="password" className="block text-terciario-white mb-2">
-        Contraseña
-      </label>
-      <input
-        type="password"
-        name="password"
-        value={data.password}
-        placeholder="Escribe tu contraseña"
-        onChange={hanldeChange}
-        className="w-full p-3 rounded-lg bg-white text-black placeholder-black"
-      />
-    </div>
-
-    <button
-      type="submit"
-      className="w-full border border-secundario text-terciario-white p-3 rounded-lg hover:bg-yellow-600">
-      Iniciar sesión
-    </button>
-  </form>
-
-  <p className="text-terciario-white text-center mt-4">
-    Si no tienes una cuenta
-    <br />
-    <Link className="text-secundario" href="/Register">
-      regístrate
-    </Link>
-  </p>
-</div>
-
   );
 };
+
 export default FormLogin;
