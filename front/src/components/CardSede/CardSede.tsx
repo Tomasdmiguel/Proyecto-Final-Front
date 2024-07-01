a2bd1e3d3081f7356843508698116d931b050c5f
psmeier <psmeier05@gmail.com>
2024-07-01 01:41:34 -0300

    asi le anda a javi

--- front/src/components/CardSede/CardSede.tsx
+++ front/src/components/CardSede/CardSede.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import Link from "next/link";
 import { useSport } from "@/context/SportContext";
 
-const CardSede = ({ name, location, description, id }: ISede) => {
+const CardSede = ({ name, location, description, id, imgUrl }: ISede) => {
   const { sport } = useSport();
   return (
     <div
@@ -17,18 +17,20 @@ const CardSede = ({ name, location, description, id }: ISede) => {
             ? "hover:bg-orange-500 shadow-orange-500"
             : "hover:bg-main shadow-main"
         }
-        w-full min-h-fit font-Marko text-lg flex flex-col rounded-md shadow-lg hover:text-white hover:shadow-xl  transition duration-300 ease-in-out p-4 sm:p-6 md:p-8 space-y-4 sm:space-y-6 md:space-y-8`}
+        w-full min-h-fit font-Marko text-lg flex flex-row rounded-md shadow-lg hover:text-white hover:shadow-xl  transition duration-300 ease-in-out p-4 sm:p-6 md:p-8 space-y-4 sm:space-y-6 md:space-y-8`}
     >
-      <h2 className=" font-bold text-2xl sm:text-3xl">{name}</h2>
-      <p>Dirección: {location}</p>
-      <p>Descripción: {description}</p>
-
-      <Link
-        href={`/sede/${id}`}
-        className="px-4 py-2 w-fit self-end bg-terciario text-terciario-white font-bold rounded-md  transition duration-300 ease-in-out sm:mt-6 sm:px-6 sm:py-3 md:mt-8 md:px-8 md:py-4 hover:bg-terciario-white hover:text-terciario hover:shadow-sm hover:shadow-terciario"
-      >
-        Ver canchas
-      </Link>
+      <img className="size-60 rounded-full" src={imgUrl} alt="" />
+      <div className="h-full flex flex-col items-center w-full ">
+        <h1 className="font-black text-2xl uppercase">{name} </h1>
+        <p>{location} </p>
+        <p>Descripción: {description} </p>
+        <Link
+          href={`/sede/${id}`}
+          className="px-4 py-2 w-fit self-end bg-terciario text-terciario-white font-bold rounded-md  transition duration-300 ease-in-out sm:mt-6 sm:px-6 sm:py-3 md:mt-8 md:px-8 md:py-4 hover:bg-terciario-white hover:text-terciario hover:shadow-sm hover:shadow-terciario"
+        >
+          Ver canchas
+        </Link>
+      </div>
     </div>
   );
 };
