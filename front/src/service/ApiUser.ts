7481af1b6aaae7ee0fd3ee8b4303ae9bfca6f951
psmeier <psmeier05@gmail.com>
2024-07-01 19:20:06 -0300

    + cambios

---
+++ front/src/service/ApiUser.ts
@@ -0,0 +1,15 @@
+const apiKey = process.env.NEXT_PUBLIC_API_URL;
+
+export const fetchUserById = async (id: string | undefined) => {
+  try {
+    const response = await fetch(`${apiKey}/user/${id}`, {
+      method: "GET",
+      next: { revalidate: 0 },
+    });
+
+    const user = await response.json();
+    return user;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
