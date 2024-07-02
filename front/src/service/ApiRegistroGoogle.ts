65064977dbb6a1937ec06deed9d9a88680c7c468
tomasdmiguel <tomasdeskate@hotmail.es>
2024-07-02 17:49:16 -0300

    Autenticacion de terceros terminado

--- front/src/service/ApiRegistroGoogle.ts
+++ front/src/service/ApiRegistroGoogle.ts
@@ -13,14 +13,14 @@ export const PostRegistroGoogle = async(data:any) => {
     console.log(dataGoogle);
       if (response.ok) {
         console.log("Usuario registrado correctamente");
-        // Aquí puedes manejar cualquier lógica adicional después de registrar al usuario
+    
       } else {
         const errorMessage = await response.text();
         console.error("Error al registrar usuario:", errorMessage);
-        // Aquí puedes manejar el error en caso de que falle el registro
+        
       }
     } catch (error:any) {
       console.error("Error inesperado:", error.message);
-      // Aquí puedes manejar errores que ocurran durante la ejecución de la petición
+      
     }
 }
