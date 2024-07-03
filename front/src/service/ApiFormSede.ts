7b3b6ff0ca005d47382a1dc8134702802dace9bd
tomasdmiguel <tomasdeskate@hotmail.es>
2024-07-03 10:52:21 -0300

    Arreglando las variables de entoncer y apiFormSede

--- front/src/service/ApiFormSede.ts
+++ front/src/service/ApiFormSede.ts
@@ -1,16 +1,21 @@
 import { IFormSede } from "@/interface/IFormSede";
 import { IUser } from "@/interface/IUser";
 
+
 const apiKey = process.env.NEXT_PUBLIC_API_URL;
 
-export const fetchFormSede = async (file: File, data: IFormSede, userDB: IUser) => {
+export const fetchFormSede = async (
+  file: File,
+  data: IFormSede,
+  userDB: IUser
+) => {
   try {
     const formData = new FormData();
     formData.append("file", file);
-    formData.append("name", data.name);
+    formData.append("data", data.name);
     formData.append("location", data.location);
     formData.append("description", data.description);
-    formData.append("user", userDB.id); 
+    formData.append("user", userDB.userDb.id);
 
     const response = await fetch(`${apiKey}/sede`, {
       method: "POST",
