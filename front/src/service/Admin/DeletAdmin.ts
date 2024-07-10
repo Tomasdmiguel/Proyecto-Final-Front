66a78afae2e9d6e808179024570a7886fff40760
tomasdmiguel <tomasdeskate@hotmail.es>
2024-07-10 18:35:16 -0300

    Dashboard panel de admnin

---
+++ front/src/service/Admin/DeletAdmin.ts
@@ -0,0 +1,44 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const deleteCancha = async (id: string): Promise<void> => {
+  try {
+    const response = await fetch(`${API_URL}/cancha/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(`Error eliminando la cancha: ${errorData.message}`);
+    }
+
+    console.log("Cancha eliminada correctamente");
+  } catch (error) {
+    console.error("Error eliminando la cancha:", error);
+    throw error;
+  }
+};
+
+export const deleteSede = async (token: string, id: string): Promise<void> => {
+    try {
+      const response = await fetch(`${API_URL}/sede/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`, // Incluir el token de autenticación
+        },
+      });
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(`Error eliminando la sede: ${errorData.message}`);
+      }
+  
+      console.log('Sede eliminada correctamente');
+    } catch (error) {
+      console.error('Error eliminando la sede:', error);
+      throw error;
+    }
+  };
