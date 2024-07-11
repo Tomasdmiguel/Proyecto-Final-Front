3d8c578b23fa8da51e938b15e537e6f18588cbaf
Javierc97 <javi.c97@hotmail.com>
2024-07-10 21:17:29 -0300

    create api get user by email y api registro google

---
+++ front/src/service/Superadmin/ApiGetUserByEmail.ts
@@ -0,0 +1,19 @@
+export const FetchUserByEmail = async (email: string) => {
+  const apiKey = process.env.NEXT_PUBLIC_API_URL;
+
+  try {
+    const res = await fetch(`${apiKey}/email/obtiene/usuario/${email}`, {
+      method: "GET",
+    });
+    if (!res.ok) {
+      throw new Error(
+        `Error al obtener el usuario ${email}. Status: ${res.status}`
+      );
+    }
+
+    const turno = await res.json();
+    return turno;
+  } catch (error: any) {
+    throw new Error(`Error en FetchUserByEmail: ${error.message}`);
+  }
+};
